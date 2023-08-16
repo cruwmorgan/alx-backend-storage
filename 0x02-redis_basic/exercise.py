37a84928fde251cace9ff31170c64d828c54a1c1
cruwmorgan <morganokeke@gmail.com>
2023-08-16 06:14:48 -0700

    Writing strings to Redis

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -2,14 +2,19 @@
 from uuid import uuid4
 import redis
 from typing import Union, Callable
+from functools import wraps
 
 
 class Cache:
+    """Functionalities"""
+
     def __init__(self):
         """ Redis Instance """
         self._redis = redis.Redis()
         self._redis.flushdb()
 
+    @call_history
+    @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """Store the cache
 
