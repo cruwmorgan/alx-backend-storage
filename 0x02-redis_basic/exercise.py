fd489446fcfa977378f49ce2f08aa92d544fe499
cruwmorgan <morganokeke@gmail.com>
2023-08-16 06:15:42 -0700

    Writing strings to Redis

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -13,8 +13,6 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
-    @call_history
-    @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """Store the cache
 
